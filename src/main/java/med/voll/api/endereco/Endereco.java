fdb636b76001452fa941e0075553e6637482081e
julio <julio.alemao8825@gmail.com>
2023-07-10 12:38:26 -0300

    Bean Validation

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,31 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String uf;
+    private String cidade;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cep = dados.cep();
+        this.bairro = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+    }
+}
---
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -0,0 +1,17 @@
+create table medicos(
+
+    id bigint not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    crm varchar(60) not null unique,
+    especialidade varchar(100) not null,
+    logradouro varchar(120) not null,
+    bairro varchar(100) not null,
+    cep varchar(20) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null,
+
+    primary key(id)
+);
