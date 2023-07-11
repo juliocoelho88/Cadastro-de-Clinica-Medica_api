2cfd6d213e741970f987e44e82d42367cf6aadd5
julio <julio.alemao8825@gmail.com>
2023-07-11 11:02:37 -0300

    testei o metodo listar no insominia e funcionou.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -18,5 +21,9 @@ public class MedicoController {
     @Transactional
     public void cadastar(@RequestBody @Valid DadosCadastroMedicos dados){
         repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
